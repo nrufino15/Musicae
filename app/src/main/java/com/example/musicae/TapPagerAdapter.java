45b40df27857286b4bc861afed350186e1a7d4ba
nrufino15 <nrufino@elpuig.xeill.net>
2019-03-17 20:50:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/musicae/TapPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.musicae;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TapPagerAdapter extends FragmentStatePagerAdapter {
+
+    String[] tabarray = new String[]{"MusicFragment","PlayList"};
+
+    public TapPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabarray[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new MusicFragment();
+            case 1: return new PlayListFragment();
+            default: return new MusicFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
